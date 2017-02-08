a002f9558a0c0408fcd85c20c71915944e22fc5b
qiulanzhu <2522324556@qq.com>
2017-02-08 12:05:25 +0800

    检查用户是否登陆

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var user = req.session.user;
-   res.render('main', { username:user.username});
+
+  if(_.isUndefined(user)){
+    res.json({
+      msgCode: -1,
+      message: '用户未登陆'
+    });
+  }else {
+    res.json({
+      msgCode: 0,
+      message: { username:user.username}
+    });
+  }
 });
 
 module.exports = router;
